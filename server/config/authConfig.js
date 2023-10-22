4a35ef3f9dd6d533fcd56f0c8ce1b4a652f298f8
fefiy <foziayimam87@gmail.com>
2023-10-22 20:35:39 +0300

    add vercel config perisma

--- server/config/authConfig.js
+++
@@ -1,9 +0,0 @@
-const {auth} = require('express-oauth2-jwt-bearer')
-
-const jwtCheck = auth({
-    audience: "http://localhost:4200",
-    issuerBaseURL: "https://dev-rjj5bt2epjmnc3j6.us.auth0.com/api/v2/",
-    tokenSigningAlg: "RS256",
-})
-
-module.exports = jwtCheck
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const jwtCheck = require("../config/authConfig")
+
 
 const {
     bookVisit,
